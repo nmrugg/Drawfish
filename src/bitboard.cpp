aefabaa70f258fcbefa7f2cfcc50f64a1a00d224
root <root@qemu-x86-64>
2015-01-10 14:22:16 +0000

    Reduce

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -2,19 +2,19 @@
 
 Bitboard FileBB[8];
 Bitboard AdjacentFilesBB[8];
-Bitboard PawnAttackSpan[2][64];
+Bitboard C;
 
 /// Bitboards::init() initializes various bitboard tables. It is called at
 /// startup and relies on global objects to be already zero-initialized.
 
 void Bitboards::init() {
 
-  for (File f = FILE_A; f <= FILE_H; ++f)
-      FileBB[f] = f > FILE_A ? FileBB[f - 1] << 1 : 0x01010101;
+  for (int f = 0; f <= 7; ++f)
+      FileBB[f] = f > 0 ? FileBB[f - 1] << 1 : 0x01010101;
 
-  for (File f = FILE_A; f <= FILE_H; ++f)
-      AdjacentFilesBB[f] = (f > FILE_A ? FileBB[f - 1] : 0) | (f < FILE_H ? FileBB[f + 1] : 0);
+  for (int f = 0; f <= 7; ++f)
+      AdjacentFilesBB[f] = (f > 0 ? FileBB[f - 1] : 0) | (f < 7 ? FileBB[f + 1] : 0);
 
-  PawnAttackSpan[0][0] = AdjacentFilesBB[7];
+  C = AdjacentFilesBB[7];
 }
 
