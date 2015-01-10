32ff92de2f6307f5b489f646ceedc432c71662d6
root <root@qemu-x86-64>
2015-01-10 14:10:48 +0000

    Further reduce

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -14,15 +14,8 @@ void Bitboards::init() {
   for (File f = FILE_A; f <= FILE_H; ++f)
       FileBB[f] = f > FILE_A ? FileBB[f - 1] << 1 : FileABB;
 
-  for (Rank r = RANK_1; r <= RANK_8; ++r)
-      RankBB[r] = r > RANK_1 ? RankBB[r - 1] << 8 : Rank1BB;
-
   for (File f = FILE_A; f <= FILE_H; ++f)
       AdjacentFilesBB[f] = (f > FILE_A ? FileBB[f - 1] : 0) | (f < FILE_H ? FileBB[f + 1] : 0);
-
-  for (Rank r = RANK_1; r < RANK_8; ++r)
-      InFrontBB[WHITE][r] = ~(InFrontBB[BLACK][r + 1] = InFrontBB[BLACK][r] | RankBB[r]);
-
 
   PawnAttackSpan[0][0] = AdjacentFilesBB[7];
 }
