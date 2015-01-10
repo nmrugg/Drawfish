d399001462468e31f994d1327930eaed7f0b08e1
root <root@qemu-x86-64>
2015-01-10 12:10:49 +0000

    Retire FORCE_INLINE

--- src/types.h
+++ src/types.h
@@ -13,14 +13,6 @@ const bool HasPopCnt = true;
 
 #define CACHE_LINE_SIZE 64
 
-#ifdef _MSC_VER
-#  define FORCE_INLINE  __forceinline
-#elif defined(__GNUC__)
-#  define FORCE_INLINE  inline __attribute__((always_inline))
-#else
-#  define FORCE_INLINE  inline
-#endif
-
 #ifdef IS_64BIT
 const bool Is64Bit = true;
 #else
